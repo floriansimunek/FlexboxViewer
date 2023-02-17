d67e0c582b545917564bc218e26d172dc1a066e4
Florian SIMUNEK <florian.simunek@gmail.com>
2023-02-18 00:21:56 +0100

    :lipstick: Modal style is prettier

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const SIDEBAR_BUTTON = document.querySelector(".sidebar__button");
 SIDEBAR_BUTTON.addEventListener("click", () => {
 	SIDEBAR.classList.toggle("expanded");
 	SIDEBAR_BUTTON.classList.toggle("expanded");
-	VIEW.classList.toggle("expanded");
 });
 
 const ADD_BLOCK_BUTTON = document.querySelector(".addBlock");
@@ -15,7 +14,6 @@ const ADD_BLOCK_MODAL = document.querySelector(".modal__addBlock");
 const PARENT_WIDTH = document.querySelector("#parentWidth");
 const PARENT_HEIGHT = document.querySelector("#parentHeight");
 const PARENT_PADDING = document.querySelector("#parentPadding");
-const PARENT_BORDER = document.querySelector("#parentBorder");
 
 const SUBMIT_INPUTS = document.querySelector(".submitParentBlock");
 
@@ -28,7 +26,7 @@ SUBMIT_INPUTS.addEventListener("click", () => {
 	PARENT.style.width = PARENT_WIDTH.value + "%";
 	PARENT.style.height = PARENT_HEIGHT.value + "%";
 	PARENT.style.padding = PARENT_PADDING.value + "px";
-	PARENT.style.border = PARENT_BORDER.value;
+	PARENT.style.border = "10px solid #0065FF";
 
 	VIEW.append(PARENT);
 	ADD_BLOCK_MODAL.classList.toggle("open");
